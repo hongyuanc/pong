322d2252c42ecdfe536544ba43f5cce13109a619
Hong <hong.cao0824@gmail.com>
2024-02-26 22:49:02 -0500

    setup shapes and colors for objects

--- main.py
+++ main.py
@@ -1,20 +1,32 @@
-import pygame, sys
+import pygame as pg
+import sys
 
 # setup
-pygame.init()
-clock = pygame.time.Clock()
+pg.init()
+clock = pg.time.Clock()
 
 # setting up the window
 width = 1280
 height = 920
-screen = pygame.display.set_mode((width, height))
-pygame.display.set_caption("Pong")
+screen = pg.display.set_mode((width, height))
+pg.display.set_caption("Pong")
+
+ball = pg.Rect(width/2 - 15, height/2 - 15, 30, 30)
+player = pg.Rect(width - 20, height/2 - 70, 10, 140)
+opponent = pg.Rect(10, height/2 - 70, 10, 140)
+
+# adding color
+screen.fill(pg.Color('Silver'))
+pg.draw.ellipse(screen, pg.Color('Black'), ball)
+pg.draw.rect(screen, pg.Color('Grey9'), player)
+pg.draw.rect(screen, pg.Color('Grey9'), opponent)
+pg.draw.aaline(screen, pg.Color('Grey9'), (width/2, 0), (width/2, height))
 
 while True:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            pygame.quit()
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            pg.quit()
             sys.exit()
 
-    pygame.display.flip()
+    pg.display.flip()
     clock.tick(60)
