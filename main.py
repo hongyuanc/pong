58c3f612b7d7a072f213b82eab27044c30c22b92
Hong <hong.cao0824@gmail.com>
2024-02-26 22:36:37 -0500

    finished setup

--- main.py
+++ main.py
@@ -1,3 +1,20 @@
 import pygame, sys
 
+# setup
 pygame.init()
+clock = pygame.time.Clock()
+
+# setting up the window
+width = 1280
+height = 920
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Pong")
+
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    pygame.display.flip()
+    clock.tick(60)
