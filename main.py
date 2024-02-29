271d7f645f310c3ab6358b89b32bb9fe22de1425
Hong <hong.cao0824@gmail.com>
2024-02-29 16:16:35 -0500

    implementing score function - however it does not increment

--- main.py
+++ main.py
@@ -12,6 +12,11 @@ height = 920
 screen = pg.display.set_mode((width, height))
 pg.display.set_caption("Pong")
 
+font = pg.font.Font(None, 36)
+
+score_a = 0
+score_b = 0
+
 def ball_animation():
     global ball_speed_x, ball_speed_y
     # sets speed to the ball
@@ -49,12 +54,22 @@ def restart():
     ball_speed_y = random.choice((1, -1))
     ball_speed_x = random.choice((1, -1))
 
+def score():
+    global score_a, score_b
+
+    if ball.left <= 0:
+        score_b += 1
+    if ball.right >= width:
+        score_a += 1
+
 # shape and its dimentions
 ball = pg.Rect(width/2 - 15, height/2 - 15, 30, 30)
 player = pg.Rect(10, height/2 - 70, 10, 140)
 opponent = pg.Rect(width - 20, height/2 - 70, 10, 140)
 
 
+
+# speed parameters
 ball_speed_x = 7
 ball_speed_y = 7
 player_speed = 0
@@ -80,13 +95,23 @@ while True:
     ball_animation()
     player_animation()
     opponent_animation()
-
+   
     # adding color
     screen.fill(pg.Color('Silver'))
     pg.draw.ellipse(screen, pg.Color('Black'), ball)
     pg.draw.rect(screen, pg.Color('Grey9'), player)
     pg.draw.rect(screen, pg.Color('Grey9'), opponent)
     pg.draw.aaline(screen, pg.Color('Grey9'), (width/2, 0), (width/2, height))
+
+    # implementing scores
+    score_text_a= font.render(f"{score_a}", True, pg.Color('Black'))
+    score_text_b= font.render(f"{score_b}", True, pg.Color('Black'))
+    screen.blit(score_text_a, (width/4,50))
+    screen.blit(score_text_b, (width/4 + width/2,50))
+    score()
+
+
+
 
     pg.display.flip()
     clock.tick(60)
